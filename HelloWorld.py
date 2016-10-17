8675f7195d51d380f944edf1c0164ff1dba7116e
Vanessa Ulloa <vulloa@csumb.edu>
2016-10-17 12:24:19 -0700

    add GuessTheNumber program

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+
+
+def spam(divideBy) :
+    try :
+        return 42/divideBy
+    except ZeroDivisionError :
+        print('Error: invalid argument.')
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
